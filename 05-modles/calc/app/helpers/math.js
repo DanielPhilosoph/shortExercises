f880e7005ac2c9e40b227c419428bc6049febf7b
DanielPhilosoph <danielph9@gmail.com>
2021-10-17 15:38:01 +0300

    basic structure and operate

---
+++ 05-modles/calc/app/helpers/math.js
@@ -0,0 +1,26 @@
+import { ModuleFilenameHelpers } from "webpack";
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function sub(n1, n2) {
+  return n1 - n2;
+}
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+function divide(n1, n2) {
+  return n1 / n2;
+}
+function equals(n1, n2, callback) {
+  return callback(n1, n2);
+}
+
+export default {
+  add,
+  sub,
+  multiply,
+  divide,
+  equals,
+};
--- 05-modles/calc/app/main.js
+++ 05-modles/calc/app/main.js
@@ -1,1 +1,46 @@
-import "./styles.css";
+//import "./styles.css";
+import { equals } from "./helpers/math.js";
+
+function onDigitClick(event) {
+  let resultInput = document.querySelector("#result");
+  resultInput.value += event.target.value;
+}
+
+function onOperatorClick(event) {
+  let resultInput = document.querySelector("#result");
+  resultInput.value += " " + event.target.value + " ";
+}
+
+function onDelClick() {
+  document.querySelector("#result").value = "";
+}
+
+function onEqualsClick(event) {
+  let resultInput = document.querySelector("#result");
+  let calcString = resultInput.value;
+  let calcArray = calcString.split(" ");
+  if (calcArray.length > 3) {
+    resultInput.value = "Only 1 operator";
+  } else {
+    resultInput.value = equals(calcArray[0], calcArray[1], calcArray[2]);
+  }
+}
+
+function main() {
+  document.querySelector("#equals").addEventListener("click", onEqualsClick);
+  document.querySelector("#del").addEventListener("click", onDelClick);
+
+  let operatorArray = ["Plus", "Present", "Divide", "Times", "Minus", "Dot"];
+  for (let i = 0; i < operatorArray.length; i++) {
+    document
+      .querySelector(`#operator${operatorArray[i]}`)
+      .addEventListener("click", onOperatorClick);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    document
+      .querySelector(`#digit${i}`)
+      .addEventListener("click", onDigitClick);
+  }
+}
+main();
